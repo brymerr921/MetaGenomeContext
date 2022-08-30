d8e30ab433d78cab6c09fff07b74a9398b0782f1
brymerr921 <brymerr921@gmail.com>
2022-08-30 00:46:00 -0700

    Initial commit, works through metagenome assembly

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MetaGenomeContext
 This pipeline can be used to assemble metagenomes and extract contigs containing a protein with a specific function, as well as surrounding genes.
+
+## Setup
+To run on a local machine, set up the following conda environment:
+```
+conda create -n mgc -c conda-forge -c bioconda -c nodefaults sra-tools pigz pbzip2 snakemake-wrapper-utils snakemake-minimal bbmap spades python==3.9 fasta-splitter prodigal
+```
---
+++ run_pipeline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+export accession=ERR1136736
+export coreNum=12
+export mem_mb=64000
+export mem_gb=64
+
+# The wrapper version of metaspades won't utilize all threads 
+# unless this variable is set:
+export OMP_NUM_THREADS=$coreNum
+
+echo "Num threads == $OMP_NUM_THREADS"
+
+snakemake -p -j $coreNum --resources mem_mb=$mem_mb -s /home/bmerrill/user_data/Projects/github_projects/MetaGenomeContext/Snakefile_MGC --config accession=$accession all_cpu=$coreNum mem_gb=$mem_gb skip_ec='--only-assembler' --configfile /home/bmerrill/user_data/Projects/github_projects/MetaGenomeContext/config.yaml #-n --debug-dag
